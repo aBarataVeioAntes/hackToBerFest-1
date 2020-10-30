9a4a2a31cf453dc6f59c102d78de7bc92e21c200
Erlitonlucas <erlitonfalucas@gmail.com>
2020-10-30 15:57:33 -0300

    1° - commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var app = require('http').createServer(resposta);
+var fs = require('fs');
+app.listen(3000);
+console.log('o pai ta on...');
+function resposta(req, res){
+   fs.readFile(__dirname + '/index.html', (err, data) => {
+	   if(err){
+	       res.writeHead(500);
+	       return res.end('Erro ao carregar o arquivo');
+	   }
+           res.writeHead(200);
+	   res.end(data);
+	   
+   });
+}
+
+/*var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.get('/', function (req, res) {
+     fs.readFile('./index.html', (err, data) =>{
+          if(err){
+               res.send('Erro ao carregar a pagina');
+     } else{
+               res.send(data);
+     }
+})
+});
+
+app.listen(3000, function(){
+     console.log('Exemple app listening on port 3000!');
+});*/
